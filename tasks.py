070f81dbf2418b902a0212ce5f4ff17b915f1de3
Kalle Ilves <kalleilv@gmail.com>
2022-03-16 08:36:30 +0200

    pty

--- tasks.py
+++ tasks.py
@@ -8,29 +8,29 @@ def start(ctx):
 
 @task
 def build(ctx):
-    ctx.run("python3 src/build.py")
+    ctx.run("python3 src/build.py", pty=True)
 
 
 @task
 def test(ctx):
-    ctx.run("pytest src")
+    ctx.run("pytest src", pty=True)
 
 
 @task
 def lint(ctx):
-    ctx.run("pylint src")
+    ctx.run("pylint src", pty=True)
 
 
 @task
 def format(ctx):  # pylint: disable=redefined-builtin
-    ctx.run("autopep8 --in-place --recursive src")
+    ctx.run("autopep8 --in-place --recursive src", pty=True)
 
 
 @task
 def coverage(ctx):
-    ctx.run("coverage run --branch -m pytest src")
+    ctx.run("coverage run --branch -m pytest src", pty=True)
 
 
 @task(coverage)
 def coverage_report(ctx):
-    ctx.run("coverage html")
+    ctx.run("coverage html", pty=True)
